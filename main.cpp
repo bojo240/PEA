7344f8d2bc441b237eb3ea4e96693dbe8935814b
bojo240 <bojo240@gmail.com>
2018-11-16 20:16:01 +0100

    kurwa dziala szmata jebana.
    asd

--- main.cpp
+++ main.cpp
@@ -56,22 +56,26 @@ void recurency(int currentdistance, bool seen[], std::string path, int present, 
     for(int i=0;i<cityamount;++i)//sprawdzam czy jest jeszcze jakies nieodwiedzone miasto
         if(copyseen[i]==false)
         {
-            std::cout<<"\n\ndodaje dystans do currentdistance:distances["<<present<<"]["<<i<<"].\n";
             currentdistance+=distances[present][i]; // dodaje odleglosc z miasta obecnego do nastepnego
-            std::cout<<"obecnie currendistance: "<<currentdistance<<"\n";
             previous = present;//w kolejnym poziomie rekurencji, poprzednie miasto bedzie obecnym
             present = i; // a miasto do ktorego bede szedl bedzie w kolejnym poziomie rekurencji obecnym
             copypath += 'A'+present; // dodaje obecne miasto do listy odwiedzonych juz miast
-            std::cout<<'\n'<<copypath<<"\n\n";
+            std::cout<<"\ncopypath:"<<copypath<<"\n\n";
+            //std::cout<<"path:"<<path<<"\n\n";
             recurency(currentdistance, copyseen, copypath, present, previous); //wejdz do kolejnego miasta
+            copypath = path;
         }
-    if(copypath.length()==cityamount)//jezeli jestes na najnizszym z mozliwych poziomow rekurencji
+        //tutaj po powrocie z rekurencji z 4 poziomu na 3 moge sobie wszystko wypisac
+
+
+
+    if(path.length()==cityamount)//jezeli jestes na najnizszym z mozliwych poziomow rekurencji
     {
-        std::cout<<"\n\njestem w drugim ifie. copypath.length() = 4, dodaje do niego copypath[0] i w tym momencie wyglada tak:";
+        std::cout<<"\n\njestem w drugim ifie. copypath.length() = 4, copypath:";
         copypath += copypath[0];        //dodaje pierwsze miasto do sciezki
         char c = copypath[0];
         std::cout<<copypath<<'\n';
-        std::cout<<"\ndodaje teraz dystans z miasta ostatniego do pierwszego, czyli distances["<<present<<"]["<<static_cast<int>(c-'A')<<"]."<<'\n';
+        std::cout<<"\ndodaje dystans z miasta ostatniego do pierwszego, czyli distances["<<present<<"]["<<static_cast<int>(c-'A')<<"]."<<'\n';
         currentdistance += distances[present][static_cast<int>(c-'A')];
         std::cout<<"w tym momencie currentdistance wynosi:"<<currentdistance<<"\n\nDotad jest git\n\n";
         if(currentdistance<shortestdistance) //jezeli ta sciezka jest krotsza od najkrotszej dotychczas znalezionej, zamien wartosc najkrotszej sciezki na krotsza
@@ -79,7 +83,7 @@ void recurency(int currentdistance, bool seen[], std::string path, int present, 
             shortestdistance=currentdistance;
             shortestpath=copypath;
         }
-        std::cout<<"\nNajkrotsza sciezka w tym momencie to "<<path<<'\n';
+        std::cout<<"\nNajkrotsza sciezka w tym momencie to "<<copypath<<'\n';
     }
     delete[] copyseen;
 }
